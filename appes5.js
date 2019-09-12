c46484cd3b1084fc51847d813c4b9e263c859a5c
katherinebl <katherine.briceno@kairosds.com>
2019-09-12 09:59:02 +0200

    Created add book to list function

--- appes5.js
+++ appes5.js
@@ -8,6 +8,22 @@ function Book(title, author, isbn) {
 //UI Constructor, add, delete, show alert...
 function UI() { }
 
+//Add book to list
+UI.prototype.addBookToList = function (book) {
+    const list = document.getElementById('book-list');
+    //Create tr element
+    const row = document.createElement('tr');
+    //Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">x</a></td>
+    `;
+
+    list.appendChild(row);
+}
+
 //Event Listeners
 document.getElementById('book-form').addEventListener('submit',
     function (e) {
@@ -21,6 +37,9 @@ document.getElementById('book-form').addEventListener('submit',
 
         //Instantiate ui
         const ui = new UI();
+
+        //Add book to list 
+        ui.addBookToList(book);
 
         e.preventDefault();
     });
